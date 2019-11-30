a219917997d74617f1b29ac47293992c3f3d3f88
Yuliya <harmful.sun@gmail.com>
2019-11-30 18:55:41 +0300

    feat: add log with winston

--- lesson-5/src/index.js
+++ lesson-5/src/index.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
+const winston = require('winston');
+const express = require('express');
+const app = express();
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const express = require('express');
-const app = express();
-
-let idCounter = 10;
+let idCounter = 4;
 
 const newsFile = path.join(__dirname, './data/news.json');
+const logFile = path.join(__dirname, 'logfile.log');
+
+fs.unlinkSync(logFile);
 
 app.set('view engine', 'pug');
 app.set('views','./src/views');
@@ -24,17 +27,36 @@ const readNewsFile = async () => {
   return JSON.parse(data);
 };
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: logFile, timestamp: true })
+  ]
+});
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  const { url } = req;
+  logger.log('info', url);
+  next();
+})
 
 app.use((error, req, res, next) => {
   res.render('index', { title: 'Error', message: `${error.message}` });
   res.status(500).send('Something went wrong!');
 });
 
-// app.get('/', (req, res) => {
-//   res.render('index', { title: 'Main', message: 'Some news APi'});
-//   res.status(200).send('OK');
-// });
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Main', message: 'Some news APi'});
+  res.status(200).send('OK');
+});
 
 app.get('/news', async (req, res, next) => {
   try {
@@ -50,7 +72,7 @@ app.get('/news/:id', async (req, res) => {
   try {
     const news = await readNewsFile();
     const { params: { id: newsId} } = req;
-    const targetNews = news.filter(item => item.id === +newsId);
+    const targetNews = news.filter(({ id }) => id === +newsId);
     if (targetNews.length !== 0) {
       res.json(targetNews);
     } else {
@@ -65,7 +87,7 @@ app.get('/news/:id', async (req, res) => {
 app.post('/news', async (req, res) => {
   try {
     const news = await readNewsFile();
-    const { source, title } = req.body;
+    const { body: {source, title} } = req;
     news.push({
       id: idCounter++,
       source,
@@ -83,9 +105,9 @@ app.put('/news/:id', async (req, res) => {
   try {
     const news = await readNewsFile();
     const { params: { id: newsId} } = req;
-    const targetNews = news.find(item => item.id === +newsId);
+    const targetNews = news.find(({ id }) => id === +newsId);
     if (targetNews) {
-      const { source, title } = req.body;
+      const { body: {source, title} } = req;
       if (source !== undefined) {
         targetNews.source = source;
       }
@@ -107,7 +129,7 @@ app.delete('/news/:id', async (req, res) => {
   try {
     const news = await readNewsFile();
     const { params: { id: newsId} } = req;
-    const updatedNews = news.filter(item => item.id !== +newsId);
+    const updatedNews = news.filter(({ id }) => id !== +newsId);
     if (updatedNews.length !== 0 && updatedNews.length !== news.length) {
       await updateNewsFile(updatedNews);
       res.render('index', { title: 'News', message: 'News was updated'});
