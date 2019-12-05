c098d442548d530fbfbb3810a6c6ddf4765196fb
Yuliya <harmful.sun@gmail.com>
2019-12-05 23:15:31 +0300

    chore: add packages and configs

---
+++ lesson-7/webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+
+      {
+        test: /\.css$/,
+        exclude: /\.module.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.module.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
